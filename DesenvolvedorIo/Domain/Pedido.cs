91647f03ad24f55b54cf473497d3d5f59436cb04
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-08-04 14:41:07 -0300

    Criando as classes

---
+++ DesenvolvedorIo/Domain/Pedido.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfCore.Domain
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public int ClientID { get; set; }
+        public Client Client { get; set; }
+        public DateTime IniciandoEm { get; set; }
+        public DateTime FinalizandoEm{ get; set; }
+        public TipoDeFrete TipoDeFrete{ get; set; }
+        public Status Status{ get; set; }
+        public string Observacao{ get; set; }
+
+        public List<PedidoItem>{ get; set; }
+    }
+}
