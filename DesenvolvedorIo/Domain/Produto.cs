a543e780d3b0b2612792141cef0675baab871fee
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-08-04 15:00:04 -0300

    Classes Finalizadas

---
+++ DesenvolvedorIo/Domain/Produto.cs
@@ -0,0 +1,19 @@
+﻿using EfCore.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfCore.Domain
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string CodigoDeBarras { get; set; }
+        public string Descricao { get; set; }
+        public double Valor { get; set; }
+        public TipoDoProduto TipoDoProduto { get; set; }
+        public bool Ativo{ get; set; }
+    }
+}
