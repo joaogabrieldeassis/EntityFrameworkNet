e61ef017f1ad3b6d6c409ef8a601a36e501436da
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-08-04 14:56:46 -0300

    Criando os meus value objects 1.1

---
+++ DesenvolvedorIo/ValueObjects/TipoDoProduto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfCore.ValueObjects
+{
+    public enum TipoDoProduto
+    {
+        MercadoParaVenda= 1,
+        Embalagem = 2, 
+        Servico = 3,
+    }
+}
