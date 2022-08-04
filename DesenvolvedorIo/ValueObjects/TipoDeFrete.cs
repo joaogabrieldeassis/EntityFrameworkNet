693ca5566077fa539b0b556acd4a50fedc293fb4
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-08-04 14:54:07 -0300

    Criando os meus value objects

---
+++ DesenvolvedorIo/ValueObjects/TipoDeFrete.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfCore.ValueObjects
+{
+    public enum TipoDeFrete
+    {
+        Cif = 1,
+        Fob = 2,
+        SemFrete =3,
+    }
+}
