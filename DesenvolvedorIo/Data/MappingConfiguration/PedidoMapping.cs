a5988c6c13a8a490792e70019f83745c55fc51be
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-08-05 15:48:10 -0300

    Finalizando o mapeamento

---
+++ DesenvolvedorIo/Data/MappingConfiguration/PedidoMapping.cs
@@ -0,0 +1,36 @@
+﻿using EfCore.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfCore.Data.MappingConfiguration
+{
+    public class PedidoMapping : IEntityTypeConfiguration<Pedido>
+    {
+        public void Configure(EntityTypeBuilder<Pedido> builder)
+        {
+            builder.ToTable("Pedido");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.IniciandoEm).HasDefaultValueSql("GETDATE()").ValueGeneratedOnAdd();
+            builder.Property(x => x.FinalizandoEm).HasDefaultValueSql("GETDATE()").ValueGeneratedOnAdd();
+            builder.Property(x => x.TipoDeFrete).HasConversion<string>();
+            builder.Property(x => x.Status).HasConversion<int>();
+            builder.Property(x => x.Observacao).HasColumnType("NVARCHAR(200)");
+
+        }
+    }
+}
+/*
+ *  public int Id { get; set; }
+        public int ClientID { get; set; }
+        public Client Client { get; set; }
+        public DateTime IniciandoEm { get; set; }
+        public DateTime FinalizandoEm{ get; set; }
+        public TipoDeFrete TipoDeFrete{ get; set; }
+        public StatusDoPedido Status{ get; set; }
+        public string Observacao{ get; set; }
+ * */
---
+++ DesenvolvedorIo/Data/MappingConfiguration/ProdutoMapping.cs
@@ -0,0 +1,32 @@
+﻿using EfCore.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfCore.Data.MappingConfiguration
+{
+    public class ProdutoMapping : IEntityTypeConfiguration<Produto>
+    {
+        public void Configure(EntityTypeBuilder<Produto> builder)
+        {
+            builder.ToTable("Produto");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.CodigoDeBarras).HasColumnType("NVARCHAR(150)").IsRequired();
+            builder.Property(x => x.Descricao).HasColumnType("NVARCHAR(300)").IsRequired();
+            builder.Property(x => x.Valor).IsRequired();
+            builder.Property(x => x.TipoDoProduto).HasConversion<string>();
+        }
+    }
+}
+/*
+ * public int Id { get; set; }
+        public string CodigoDeBarras { get; set; }
+        public string Descricao { get; set; }
+        public double Valor { get; set; }
+        public TipoDoProduto TipoDoProduto { get; set; }
+        public bool Ativo{ get; set; }
+ */
