9db825cd418c3f12bc9d559535701e493d36a92c
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-08-08 14:10:28 -0300

    Adicionando os DbSets

--- DesenvolvedorIo/Data/BlogDataContext.cs
+++ DesenvolvedorIo/Data/BlogDataContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EfCore.Domain;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,9 @@ namespace EfCore.Data
 {
     public  class BlogDataContext : DbContext
     {
+        public DbSet<Produto> Produtos{ get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Pedido> Pedidos{ get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
             options.UseSqlServer("Server = localhost, 1433; Database = DensenvolvedorIo; User ID = sa; Password = 1q2w3e4r@#$;Trusted_Connection=False; TrustServerCertificate=True;");
