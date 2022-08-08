a9bb22f0727cf97fc5428e170541af767a4c2403
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-08-08 14:15:40 -0300

    Adicionando a classe base

---
+++ DesenvolvedorIo/Domain/BaseId.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfCore.Domain
+{
+    public class BaseId
+    {
+        public BaseId()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+    }
+}
--- DesenvolvedorIo/Program.cs
+++ DesenvolvedorIo/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace EfCore
+﻿using EfCore.Domain;
+
+namespace EfCore
 {
     public class Program
     {
         static void Main(string[] args)
         {
 
+        }
+        public void InserirDadosNoProduto()
+        {
+            var listaDeProdutos = new List<Produto>();
         }
     }
 }
