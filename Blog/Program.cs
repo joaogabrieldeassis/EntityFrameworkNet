d3fd505b23bcb378ad32e92bee8ac4854729dbcb
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-08-12 16:08:12 -0300

    Crud básico

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -13,46 +13,33 @@ namespace Blog
         public static void ConsultarDadodos()
         {
             var context = new BlogDataContext();
-            var receiveToListInCategory = context.Users.AsNoTracking().Where(x => x.Id == 13).ToList();
-            foreach (var item in receiveToListInCategory)
+            var receiveUsers = context.Users.AsNoTracking().ToList();
+            foreach (var item in receiveUsers)
             {
-                Console.WriteLine(item.Name);
+                Console.WriteLine($"Id - {item.Id}\nName - {item.Name}");
             }
-
         }
         public static void CriarDados()
         {
             var context = new BlogDataContext();
             var user = new User
             {
-                Name = "João Gabriel",
-                Email = "joao@gmail.com",
-                PasswordHash = "apoifjap9j",
-                Image = "http>afaófk",
-                Slug = "Muito bom "
+                Name = "João Gabriel 030",
+                Email = "joaomary@gmail.com"
             };
-            context.Users.Add(user);
-            context.SaveChanges();
-
         }
         public static void AtualizarDados()
         {
-            /*var context = new BlogDataContext();
-            var produtos = context.Produtoos.FirstOrDefault(x => x.Id == 1);
-            produtos.Descricao = "Jesus eu te Amo muitaooo";
-            //context.Produtoos.Update(produtos); //(Com o update ele irá tentar atualizar todos os campos da tabela, por isso é bom usar somente o savechances)
-            context.SaveChanges();*/
             var context = new BlogDataContext();
-            var user = context.Users.Find(33);
-            user.Name = "João tem fé";
-            user.Bio = "Olha";
-            user.Slug = "slc";
+            var userUpdate = context.Users.Find(13);
+            userUpdate.Name = "João gatão";
+            //context.Update(userUpdate); Evitar de usar o update por que ele dá dois riti no banco
             context.SaveChanges();
         }
         public static void ExcluirDados()
         {
             var context = new BlogDataContext();
-            var user = new User { Id = 12 };
+            var user = new User { Id = 13 };
             context.Entry(user).State = EntityState.Deleted;
             context.SaveChanges();
         }
