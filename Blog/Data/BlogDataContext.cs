d9ee52188c9446eb5d9d3a30bf265a399b53a90c
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-08-12 07:48:40 -0300

    Configurando o dbContext

---
+++ Blog/Data/BlogDataContext.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using blog.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Blog.Data
+{
+    public class BlogDataContext : DbContext
+    {
+        private readonly ILoggerFactory _loger = LoggerFactory.Create(x => x.AddConsole());
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<PostTag> PostsTags { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserRole> UsersRoles { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseLoggerFactory(_loger).EnableSensitiveDataLogging().UseSqlServer("Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$;Trusted_Connection=False; TrustServerCertificate=True;");
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(typeof(BlogDataContext).Assembly);
+        }
+    }
+}
