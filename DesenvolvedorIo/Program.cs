69b5aa99a5d532454bfe50d7384ef3b559352567
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-10-26 13:23:13 -0300

    Mexendo com EF Core

--- DesenvolvedorIo/Program.cs
+++ DesenvolvedorIo/Program.cs
@@ -9,112 +9,35 @@ namespace EfCore
     {
         static void Main(string[] args)
         {
-            ExclusaoDeDadosComperformasse();
+            InserindoDadosNoClienteEsuasTabelasDeRelacao();
         }
         private static void AtualizandoDadosComPerfomasse()
         {
-            
-            var context = new BlogDataContext();
-            var produtos = context.Produtoos.FirstOrDefault(x => x.Id == 1);
-            produtos.Descricao = "Jesus eu te Amo muitaooo";
-            //context.Produtoos.Update(produtos); //(Com o update ele irá tentar atualizar todos os campos da tabela, por isso é bom usar somente o savechances)
-            context.SaveChanges();
-            
+
         }
         private static void InserindoDadosNoClienteEsuasTabelasDeRelacao()
         {
-            var dataContext = new BlogDataContext();
-            var dadosCliente = dataContext.Clientes.FirstOrDefault();
-            var dadosProdutos = dataContext.Produtoos.FirstOrDefault();
-           
-            var pedido = new Pedido
-            {
-                ClienteID = dadosCliente.Id,
-                IniciandoEm = DateTime.Now,
-                FinalizandoEm = DateTime.Now,
-                Observacao = "João é fiho de Deus",
-                Status = StatusDoPedido.Analise,
-                TipoDeFrete = TipoDeFrete.SemFrete,
-                PedidoItems = new List<PedidoItem>()
-                {
-                    new PedidoItem
-                    {
-                        ProdutoId = dadosProdutos.Id,
-                        Desconto = 0,
-                        Quantidade = 0,
-                        Valor = 20
-                    }
-                }
-            };
-            dataContext.Pedidos.Add(pedido);
-            dataContext.SaveChanges();
+
         }
         private static void InserirDadosNoProduto()
         {
-            var contextProduto = new BlogDataContext();
-            /*
-            var listaDeProdutos = new List<Produto>();
-            listaDeProdutos.Add(new Produto("aifhja43","Muito Bom",500,ValueObjects.TipoDoProduto.Embalagem,true));
-            contextProduto.Add(listaDeProdutos);
-            var countSaveChanges = contextProduto.SaveChanges();
-            */
-            var produto = new Produto
-            {
-                CodigoDeBarras = "12334456576778",
-                Descricao = "Muito bom",
-                Valor = 200,
-                TipoDoProduto = TipoDoProduto.Servico,
-                Ativo = false
-            };
-            contextProduto.Produtoos.Add(produto);
-            contextProduto.SaveChanges();
-            
 
         }
         private static void InserindoDadosEmGrandeQuantidade()
         {
-            var context = new BlogDataContext();
-            var produto = new Produto();
-            produto.CodigoDeBarras = "987654321";
-            produto.Descricao = "Muito mas muito bom";
-            produto.Valor = 200;
-            produto.TipoDoProduto = TipoDoProduto.Embalagem;
-            produto.Ativo = true;
 
-            //var cliente = new Client(0,"João Gabriel de assis", "Rua eusebio");
-            context.AddRange(produto);
-            var receiveSaveChabges = context.SaveChanges();
-            Console.WriteLine($"Esse é o número de registros inseridos: {receiveSaveChabges}");
         }
         private static void ConsultandoDados()
         {
-            var context = new BlogDataContext();
-            //var receiveListUser = context.Clientes.AsNoTracking().Where(x=>x.Name == "joao").ToList();
-            var receivelListProduto = context.Pedidos.Include(x=>x.PedidoItems);
-            foreach (var item in receivelListProduto)
-            {
-               
-                Console.WriteLine(item.Id);
-                Console.WriteLine(item.IniciandoEm);
-                Console.WriteLine(item.Observacao);
-                Console.WriteLine(item.ClienteID);
-            }
+
         }
         private static void InsercaoDeDadosViaCliente()
         {
-            var listClient = new List<Client>();
-            var context = new BlogDataContext();
-            Console.WriteLine("Digite o seu nome");
-            var receiveClient = Console.ReadLine();
-            var receiveClientEndereço = Console.ReadLine();
-            //listClient.Add(new Client(receiveClient,receiveClientEndereço));  
+
         }
         private static void ExclusaoDeDadosComperformasse()
         {
-            var context = new BlogDataContext();
-            var produto = new Produto { Id = 1002 };
-            context.Entry(produto).State = EntityState.Deleted;
-            context.SaveChanges();
+
         }
     }
 }
